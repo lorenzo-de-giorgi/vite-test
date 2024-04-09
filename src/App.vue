16c50cd6722ac11ab5cf73220aa8ab32714d3c3f
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-04-09 12:40:55 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <HeaderComponent/>
+    <MainComponent/>
+  </div>
+</template>
+
+<script>
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import MainComponent from './components/MainComponent.vue';
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent,
+      MainComponent
+    },
+    data(){
+      return{
+        
+      }
+    },
+    methods: {
+      
+    },
+    computed: {
+      
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  h1{
+    color: red;
+  }
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <img src="../assets/img/creazione-app-vue.png" alt="">
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
